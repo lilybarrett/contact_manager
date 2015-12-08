b679158325fb2799c0c46fc982e17eb6a0a465af
lilybarrett <lily@barrett.us>
2015-12-08 10:37:21 -0500

    initial commit, finished assignment

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :show
+end
---
+++ models/contact.rb
@@ -0,0 +1,6 @@
+class Contact < ActiveRecord::Base
+
+  def name
+    [first_name, last_name].join(' ')
+  end
+end
